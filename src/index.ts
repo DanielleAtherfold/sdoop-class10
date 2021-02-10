878c8291fd3187e144d0837fb011fe7c71c3303c
Danielle_Atherfold <datherfold16578@alumni.focuscollege.com>
2021-02-10 15:36:32 -0800

    added some classes and interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,202 @@
-// Person (is an Entity)
+type Constructor = new (...args:any[]) => {};
 
-// Family (Groups People, is an Entity)
+import { IFamily, IPerson, IBusiness, IFarm, Entity, IAnimal, IVehicle, ITrailer} from './interfaces';
 
-// Farm (is a Location, is a Business)
 
-// Business (is an Entity)
-    // has owner
 
-// Farm Animal (is an Animal, belongs to a Farm)
+// this is person class i can add a new person by writing first and last name plus his age and also we can add a pet !.
 
-// Pet (is an Animal, has a name, belongs to a person or family)
+class Person  implements IPerson {
 
-// Horse (is a Farm Animal, has a name, is a Large Animal)
+    age:number;
+    haveAPet:Entity[] = []
 
-// Cow (is a Farm Animal, is a Large Animal)
+    constructor(public firstname:string, public lastname:string, public Age:number){
 
-// Dog
+    }
 
-// Cat
+    get name() {
+        return `${this.firstname} ${this.lastname} ${this.Age}`
+    }
 
-// Truck (is a Vehicle, can tow)
+    gotAPet(pet:Entity){
+        this.haveAPet.push(pet);
 
-// Car (is a Vehicle, cannot tow)
+    }
 
-// Horse Trailer (can hold 2 large animals)
 
-// Cattle Trailer (can hold 20 Large Animals)
 
-// Small Trailer (can hold 1 large animal)
+}
+// testing person
+
+// const trying = new Person('   ', '    ', 22)
+// console.log(trying);
+
+
+
+
+// family class: it allows us to add new family members and new families plus we can add a pet !.
+
+class Family implements IFamily {
+
+    name: string
+    familyMembers: Entity[] = [];
+    haveAPet: Entity[] = [];
+
+
+
+    get displayMembers(){
+        return this.familyMembers.map(family => family.name).join(",");
+    }
+
+    addNewMember(member:Entity){
+        this.familyMembers.push(member);
+    }
+    gotAPet(pet:Entity){
+        this.haveAPet.push(pet);
+
+    }
+}
+
+// testing person and family together.
+
+// const famFam = new family()
+
+// famFam.familyName = "alshahoud";
+
+// const famFam2 = new family()
+
+// famFam2.familyName = "khan";
+
+// const trying = new Person('angham', 'alshahoud', 22)
+// const trying2 = new Person('aliah', 'alshahoud', 27)
+
+// const trying3 = new Person('jack', 'khan', 12)
+// const trying4 = new Person('mike', 'khan', 30)
+
+// famFam.addNewMember(trying)
+// famFam.addNewMember(trying2)
+// famFam2.addNewMember(trying3)
+// famFam2.addNewMember(trying4)
+
+// console.log(famFam, 'second family ' , famFam2)
+
+// this is a business class allows us to add new business and get what businesses we have.
+
+class business implements IBuissness{
+    owner_s: string;
+    BusinessName: Entity[] = [];
+
+    get Business(){
+        return this.BusinessName.map(business => business.name).join(",");
+    }
+
+    addNewBusiness(business:Entity){
+        this.BusinessName.push(business);
+    }
+
+}
+
+// testing business class by adding a family as a new business.
+
+// const newFam = new family()
+// newFam.name = 'jakijak';
+
+// const buisness1 = new buisness()
+// buisness1.owner_s = ['angham, aliah, jake'];
+
+// buisness1.addNewBusiness(newfam);
+// console.log(buisness1)
+
+// farm class:
+
+class farm implements IFarm{
+    FarmLocation: string
+    farmName: Entity[] =[];
+
+
+    get name(){
+        return ""
+    }
+
+    constructor(public nameFarm: farm["name"]){
+
+    }
+
+    addAnAnimal(animal:Entity){
+        this.farmName.push(animal)
+    }
+}
+
+//testing the farm class by adding a farm as a buisness.
+
+// const fame1 = new farm("alshahoudies")
+// fame1.FarmLocation = "1365 highway 33 west ";
+
+// const business = new business()
+
+// buisis.addNewBusiness(fame1)
+// buisis.owner_s = ["angham, me, and myself"]
+
+// console.log(business)
+
+class Animal implements IAnimal{
+    size: string;
+    isAPet: boolean;
+
+    get name(){
+        return (
+            `${this.animal_name} ${this.animalSize}`
+        )
+
+    }
+
+    constructor(public animalType: string, public animal_name: string, public animalSize: animal["size"]){
+
+    }
+}
+
+// testing with adding a pet into a person, farm, and to a family.
+
+// const newAnimal = new animal("cat","jody","small" )
+// newAnimal.isPet= true;
+// const farm2 = new farm("alshahoudies")
+// farm2.FarmLocation = "canada, B.C, KELOWNA"
+
+// const trying = new Person('angham', 'alshahoud', 22)
+
+// const newfam = new family()
+// newfam.name = 'jakijak';
+
+// farm2.addAnAnimal(newAnimal)
+// trying.gotApet(newAnimal);
+// newfam.gotApet(newAnimal)
+
+// console.log(farm2, trying, newfam)
+
+class Vehicle implements IVehicle{
+    towing:boolean;
+}
+class Truck implements IVehicle{
+    towing:true;
+}
+class Car implements IVehicle{
+    towing:false
+}
+class Trailer implements ITrailer {
+    largeAnimals: number;
+}
+class SmallTrailer implements ITrailer{
+    largeAnimals:1;
+}
+class MediumTrailer implements ITrailer {
+    largeAnimals:2;
+}
+class LargeTrailer implements ITrailer {
+    largeAnimals:20;
+}
+const horseTrailer = new LargeTrailer();
+
+const cattleTrailer = new MediumTrailer();
+
+const smallTrailer = new SmallTrailer();
---
+++ src/interfaces.ts
@@ -0,0 +1,73 @@
+export interface Entity {
+    name:string
+}
+export interface IPerson {
+    name:string;
+    age:number;
+    haveAPet: Entity [];
+
+}
+
+export interface IFamily {
+    familyMembers:Entity[];
+    name:string
+    haveAPet: Entity [];
+
+}
+
+
+
+
+// Business (is an Entity)
+// has owner
+export interface IBusiness{
+    BusinessName:Entity[];
+    owner_s:string[];
+
+}
+
+// Farm (is a Location, is a Business)
+export interface IFarm {
+
+    FarmLocation:string;
+    name:string;
+
+}
+
+export interface IAnimal {
+    size: string;
+    name:string;
+    isPet: boolean;
+}
+
+
+
+//Vehicles
+
+// Truck (is a Vehicle, can tow)
+
+// Car (is a Vehicle, cannot tow)
+
+//Trailers
+
+// Horse Trailer (can hold 2 large animals)
+
+// Cattle Trailer (can hold 20 Large Animals)
+
+// Small Trailer (can hold 1 large animal)
+
+export interface IVehicle {
+   towing:boolean;
+
+}
+
+export interface ITrailer{
+  largeAnimals:number
+}
+// export interface ICar extends IVehicle{
+//
+// }
+// export interface ITruck extends IVehicle{
+
+
+//}
